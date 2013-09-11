c65081f487e184bf8181ab053e9ae6fcd0c515af
Daniel Fagnan <dnfagnan@gmail.com>
2013-09-10 22:10:57 -0600

    tests pass
    Signed-off-by: Daniel Fagnan <dnfagnan@gmail.com>

--- test/index.js
+++ test/index.js
@@ -16,4 +16,14 @@ describe('dom', function(){
     assert(d.window instanceof Window);
   });
 
+  it('should store the parsed tree within the document', function(){
+    var d = dom('<html></html>');
+    assert(d.document.tree);
+  });
+
+  it('should store the document within the window', function(){
+    var d = dom('<html></html>');
+    assert(d.window.document);
+  });
+
 });
