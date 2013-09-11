6f593a1f4e622dcb0686d91c02b73b7c8362f9e5
Daniel Fagnan <dnfagnan@gmail.com>
2013-09-10 21:53:05 -0600

    api
    Signed-off-by: Daniel Fagnan <dnfagnan@gmail.com>

--- index.js
+++ index.js
@@ -19,8 +19,23 @@ exports = module.exports = init;
  * This parses the initial HTML input and returns the
  * appropriate `document` and `window` object.
  *
+ * Usage:
+ *
+ *  var dom = init('<html><body><div id="hello"></div></body></html>', {});
+ *
+ *  // Document Instance
+ *  dom.document
+ *
+ *  // Window Instance
+ *  dom.window
+ *
+ *  // Find ID
+ *  dom.window.getElementByID("hello"); // Returns Node
  */
 
 function init(html, options) {
+  if (!options) options = {};
+  if (!html) html = '';
 
+  return parser(html, options);
 }
