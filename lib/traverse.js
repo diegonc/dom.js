diegonc/dom.js

commit 3af1c03290d821c94ea28eb5e160f4ac63c68e54

replaced the previous node.outerHTML algorithm with a iterative depth-first search.
Signed-off-by: Daniel Fagnan <dnfagnan@gmail.com>

lib/traverse.js
@@ -95,7 +95,7 @@ function traverse(tree) {
         nodeInstance = new Text(node.textContent);
         break;
       case 8: // COMMENT
-
+        nodeInstance = new CharacterData(node.data, node.length);
         break;
       case 9: // DOCUMENT
 

lib/characterData.js
@@ -1,0 +1,28 @@
+/**
+ * Module dependencies.
+ */
+
+var util = require('util'),
+    Node = require('./node');
+
+/**
+ * Module exports
+ */
+
+exports = module.exports = CharacterData;
+
+/**
+ * Element constructor
+ */
+
+function CharacterData(data, length) {
+  Node.call(this, {});
+  this.data = data || null;
+  this.length = length || 0;
+}
+
+/**
+ * Inherits
+ */
+
+util.inherits(CharacterData, Node);

lib/document.js
@@ -5,7 +5,8 @@
 var Node = require('./node'),
     Element = require('./element'),
     Text = require('./text'),
-    util = require('util');
+    util = require('util'),
+    CharacterData = require('./characterData');
 
 /**
  * Module Exports
@@ -65,8 +66,12 @@ Document.prototype.createTextNode = function(val) {
  * `createComment`
  */
 
-Document.prototype.createComment = function() {
-
+Document.prototype.createComment = function(data) {
+  var comment = new CharacterData();
+  comment.nodeType = 8;
+  comment.data = data;
+  comment.length = data.length;
+  return comment;
 };
 
 /**

lib/node.js
@@ -52,6 +52,70 @@ function Node(options) {
 
 Object.defineProperty(Node.prototype, "outerHTML", {
   get: function() {
+
+    var discovered = [];
+    var explored = [];
+
+    var str = '';
+
+    function serializeAttributes(node) {
+      var attrs = '';
+      if (node.attributes) {
+        for (var i = 0; i < node.attributes.length; i++) {
+          var attr = node.attributes[i];
+          attrs += ' ' + attr.name + '="' + attr.value + '"';
+        }
+      }
+      return attrs;
+    }
+
+    function visit(node) {
+      discovered.push(node);
+
+      switch (node.nodeType) {
+        case 1:
+          str += '<' + node.tagName + serializeAttributes(node) + '>\n\t';
+          break;
+        case 8:
+          str += '<!-- ' + node.data + ' -->';
+          break;
+        case 3:
+          str += node.nodeValue;
+          break;
+        case 9:
+          str += '<!DOCTYPE ' + (node.doctype || 'html') + '>\n';
+          break;
+      }
+
+      if (node.childNodes) {
+        for (var i = 0; i < node.childNodes.length; i++) {
+          var e = node.childNodes[i];
+          if (explored.indexOf(e) === -1) {
+            visit(e);
+          }
+        }
+      }
+
+      switch (node.nodeType) {
+        case 1:
+          str += '\n</' + node.tagName + '>';
+          break;
+        case 8:
+          break;
+        case 3:
+          break;
+        case 9:
+          break;
+      }
+
+
+      explored.push(node);
+    }
+
+    visit(this);
+
+    return str;
+
     // XXX: totally basic impl
     var val = ['<' + this.tagName];
     if (this.attributes.length) val.push(serializeAttributes(this));

commit de1eb16552f59950fe9c953d4b2eaed6a1b1d826

lib/traverse.js: require characterData
Fix undefined reference by requiring characterData.js.

Breaking change introduced in commit 3af1c03290d821c94ea28eb5e160f4ac63c68e54.

lib/traverse.js
@@ -11,6 +11,7 @@ var Node = require('./node');
 var Element = require('./element');
 var Text = require('./text');
 var Attribute = require('./attribute');
+var CharacterData = require('./characterData');
 
 /**
  * Expose `traverse`.